1b0a96741962198bd2447acfdaa95607277443a2
shuvamk <kshuvam564@gmail.com>
2020-10-02 17:43:45 -0700

    Added My Card

--- participants.js
+++ participants.js
@@ -1,12 +1,21 @@
 const participants = [
     {
-        "id": 1,
         "name": "AIT-OSS",
         "img": "https://avatars3.githubusercontent.com/u/47062438?s=400&u=ab4cb37695763efe4297b2da4b7f97cd8f6f888b&v=4",
         "title": "Organizer",
         "year": "BE - 2022",
         "linkedIn": "https://www.linkedin.com/company/open-source-software-club/",
         "github": "https://github.com/aitoss",
+        "skills": ['All Open Source Technologies'],
+        "description": "A student driven Open Source Community at AIT, Pune."
+    }
+    {
+        "name": "Shuvam Kumar",
+        "img": "https://avatars0.githubusercontent.com/u/42249560?s=400&u=13aa2a8074b01629278602bc5ca640367ecf0b6e&v=4",
+        "title": "Participant",
+        "year": "BE - 2022",
+        "linkedIn": "https://www.linkedin.com/in/shuvam-kumar-09512a168/",
+        "github": "https://github.com/shuvamk",
         "skills": ['All Open Source Technologies'],
         "description": "A student driven Open Source Community at AIT, Pune."
     }
