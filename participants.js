f1dbea3be17078c8f49a9d89ab57aa2a6edba302
AmitBhagat3301 <amitbhagat963@gmail.com>
2020-10-03 19:38:42 +0530

    updated participant card

--- participants.js
+++ participants.js
@@ -52,7 +52,7 @@ const participants = [
     {
         "name": "Amit Bhagat",
         "img": "https://images.unsplash.com/photo-1518933165971-611dbc9c412d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-        "title": "Let's start Opensource",
+        "title": "Lets start Opensource",
         "year": "BE-2023",
         "linkedIn": "https://www.linkedin.com/in/amit-bhagat-860341192/",
         "github": "https://github.com/AmitBhagat3301",
