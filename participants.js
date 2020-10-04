aa4fbef8e23a7803dbe1d85d00db771ea5f155c5
Akshay Sharma <akshaysharma30121999@gmail.com>
2020-10-04 23:39:54 +0530

    Update participants.js

--- participants.js
+++ participants.js
@@ -124,7 +124,7 @@ const participants = [
     },{
 
         "name": "Amit Bhagat",
-        "img": "https://images.unsplash.com/photo-1518933165971-611dbc9c412d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+        "img": "https://images.unsplash.com/photo-1518933165971-611dbc9c412d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
         "title": "Lets start opensource",
         "year": "BE-2023",
         "linkedIn": "https://www.linkedin.com/in/amit-bhagat-860341192/",
